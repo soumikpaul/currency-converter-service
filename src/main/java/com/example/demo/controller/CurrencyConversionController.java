553d4f5a296ca670064d938ecbf7a59562a831a7
Soumik Paul <soumikpaulonly@gmail.com>
2020-09-05 18:31:14 +0530

    currency converter service

---
+++ src/main/java/com/example/demo/controller/CurrencyConversionController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
+	
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConverterBean converCurrentyFeign(@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quantity) {
+//		Map<String, String> uriVariables=new HashMap<>();
+//		uriVariables.put("from", from);
+//		uriVariables.put("to", to);
+//		ResponseEntity<CurrencyConverterBean> responseEntity=new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConverterBean.class,uriVariables);
+//		
+		CurrencyConverterBean response=proxy.retrieveExchangeValue(from, to);
+				
+		//		responseEntity.getBody();
+		
+		return new CurrencyConverterBean(response.getId(),from,to,response.getConversionMultiple(),quantity,quantity.multiply(response.getConversionMultiple()),response.getPort());
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(name="currency-exchange-service",url="localhost:8000")
+//@RibbonClient(name="currency-exchange-service")
+//@FeignClient(name="currency-exchange-service")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service") 
+public interface CurrencyExchangeServiceProxy {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConverterBean retrieveExchangeValue(@PathVariable("from") String from,@PathVariable("to") String to); 
+}
